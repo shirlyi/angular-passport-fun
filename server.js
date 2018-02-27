9596bd1034443947e64ebc5227362222f0804790
Shirly <shirly@Hadars-MacBook-Pro-2.local>
2018-02-27 14:22:41 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+// Get dependencies
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const app = express();
+
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+//Authentication middleware
+app.use(passport.initialize());
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist')));
+ 
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+ 
+const port = process.env.PORT || '3000';
+app.set('port', port);
+ 
+const server = http.createServer(app);
+server.listen(port, () => console.log(`API running on localhost:${port}`));
